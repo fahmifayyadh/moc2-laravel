6f12ee1c88fdd0635f2f9906d28bf2e985b2c6b1
dimpurw <terminatorid2@gmail.com>
2022-02-15 00:33:28 +0700

    fixed sidebar & wrapper

--- resources/views/V2/layouts/master.blade.php
+++ resources/views/V2/layouts/master.blade.php
@@ -31,7 +31,7 @@
     @endphp
     @include('layouts.komponen.notif')
     @endauth
-  <div id="wrapper" style="height: 100%">
+  <div id="wrapper" style="height: auto">
     <!-- Sidebar -->
     @include('V2.layouts.sidebar')
     <!-- Sidebar -->
