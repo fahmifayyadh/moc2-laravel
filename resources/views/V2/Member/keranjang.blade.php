b8490a80fd67b4cb1c1e7a05b8d7dd8b6a77b6e3
FurqonWidaya <55239325+FurqonWidaya@users.noreply.github.com>
2022-03-06 16:09:08 +0700

    Update keranjang.blade.php

--- resources/views/V2/Member/keranjang.blade.php
+++ resources/views/V2/Member/keranjang.blade.php
@@ -6,7 +6,6 @@
 @section('js')
 <script>
     $(document).ready(function() {
-       
         $.ajaxSetup({
             headers: {
                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -29,13 +28,15 @@
                 }
             });
         });
-        $('.add-cart').click(function(e){
+        //tambah produk
+        for (let i = 1; i <= 1000; ++i) {
+        $('.add-cart'+i).click(function(e){
         // get data on tag
         e.preventDefault();
-        console.log('ss');
         let id = $(this).data('id');
-        let quantity = $('#quantity').val();
-
+        let quantity = parseInt($('#quantity'+i).val());
+        $('#quantity'+i).val(quantity + 1);
+        console.log(i, id, quantity);
         // ajax request
         $.ajax({
             type:'get',
@@ -47,13 +48,15 @@
                 }
             });
         });
-        $('.min-cart').click(function(e){
+        }
+        //kurangi produk
+        for (let i = 1; i <= 1000; ++i) {
+        $('.min-cart'+i).click(function(e){
         // get data on tag
         e.preventDefault();
-        console.log('ss');
         let id = $(this).data('id');
-        let quantity = $('#quantity').val();
-
+        let quantity = parseInt($('#quantity'+i).val());
+        $('#quantity'+i).val(quantity - 1);
         // ajax request
         $.ajax({
             type:'get',
@@ -65,6 +68,8 @@
                 }
             });
         });
+        }
+
     });
 
 </script>
@@ -115,13 +120,13 @@
                             <!-- button quantity -->
                             <div class="float-right mt-3">
                                 <div class="css-h82t6w-unf-quantity-editor">
-                                    <button type="button" class="min-cart css-199ul1b quantity-left-minus" data-type="minus" data-field=""id="min-cart" data-id="{{$c->product->id}}">
+                                    <button type="button" class="min-cart{{$i+1}} css-199ul1b quantity-left-minus" data-type="minus" data-field=""id="min-cart" data-id="{{$c->product->id}}">
                                         <svg class="unf-icon" viewBox="0 0 24 24" width="18px" height="18px" fill="var(--NN300, #FF9F1C)" style="display: inline-block; vertical-align: middle;">
                                             <path d="M19 13H5c-.6 0-1-.4-1-1s.4-1 1-1h14c.6 0 1 .4 1 1s-.4 1-1 1z"></path>
                                         </svg>
                                     </button>
-                                    <input id="quantity" name="quantity" class="css-1igct5v-unf-quantity-editor__input" data-unify="QuantityEditor" type="text" value="{{$c->quantity}}" min="1" max="100">
-                                    <button type="button" class="add-cart css-199ul1b quantity-right-plus" data-type="plus" data-field=""id="add-cart" data-id="{{$c->product->id}}">
+                                    <input id="quantity{{$i+1}}" name="quantity" class="css-1igct5v-unf-quantity-editor__input" data-unify="QuantityEditor" type="text" value="{{$c->quantity}}" min="1" max="100">
+                                    <button type="button" class="add-cart{{$i+1}} css-199ul1b quantity-right-plus" data-type="plus" data-field=""id="add-cart" data-id="{{$c->product->id}}">
                                         <svg class="unf-icon" viewBox="0 0 24 24" width="18px" height="18px" fill="var(--GN500, #FF9F1C)" style="display: inline-block; vertical-align: middle;">
                                             <path d="M19 11h-6V5a1 1 0 00-2 0v6H5a1 1 0 000 2h6v6a1 1 0 002 0v-6h6a1 1 0 000-2z"></path>
                                         </svg>
